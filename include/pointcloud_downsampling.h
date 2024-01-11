8192f9bd9243dc46acc8b7c226379845042271b9
LihanChen2004 <757003373@qq.com>
2024-01-11 12:58:39 +0800

    🌈 style: Space becomes 2

--- include/pointcloud_downsampling.h
+++ include/pointcloud_downsampling.h
@@ -1,30 +1,26 @@
-#include <rclcpp/rclcpp.hpp>
 #include <rclcpp/publisher.hpp>
+#include <rclcpp/rclcpp.hpp>
 #include <rclcpp/subscription.hpp>
 #include <sensor_msgs/msg/point_cloud2.hpp>
-
-// C++ system
-#include <math.h>
 
 namespace pointcloud_downsampling
 {
 class PointcloudDownsampling : public rclcpp::Node
 {
 public:
-    explicit PointcloudDownsampling(const rclcpp::NodeOptions & options);
+  explicit PointcloudDownsampling(const rclcpp::NodeOptions & options);
 
-    ~PointcloudDownsampling() override;
+  ~PointcloudDownsampling() override;
 
 private:
-    void getParams();
+  void getParams();
 
-    void voxelFiltering(const sensor_msgs::msg::PointCloud2::UniquePtr msg);
+  void voxelFiltering(const sensor_msgs::msg::PointCloud2::UniquePtr msg);
 
-    void publish_result(sensor_msgs::msg::PointCloud2 msg);
+  void publishResult(sensor_msgs::msg::PointCloud2 msg);
 
-    rclcpp::Subscription<sensor_msgs::msg::PointCloud2>::SharedPtr pointcloud_sub_;
+  rclcpp::Subscription<sensor_msgs::msg::PointCloud2>::SharedPtr pointcloud_sub_;
 
-    rclcpp::Publisher<sensor_msgs::msg::PointCloud2>::SharedPtr pointcloud_pub_;
-
+  rclcpp::Publisher<sensor_msgs::msg::PointCloud2>::SharedPtr pointcloud_pub_;
 };
 }  // namespace pointcloud_downsampling
